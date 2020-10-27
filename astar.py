898eb294251263062b0b90b99bebcfd06d1a08c6
Cameron <cameronslee@hotmail.com>
2020-10-26 18:26:58 -0700

    added implementation for algorithm and path reconstruction

--- astar.py
+++ astar.py
@@ -7,7 +7,7 @@ WIDTH = 800
 #set up display
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 #caption for display
-pygame.display.set_caption("A* Path finding Algorithm")
+pygame.display.set_caption("A* Pathfinding Algorithm")
 
 #color def
 RED = (255, 0, 0)
@@ -30,7 +30,7 @@ class Spot:
         self.col = col
         self.x = row * width
         self.y = col * width
-        self.color = WHITE
+        self.color = BLACK
         self.neighbors = []
         self.width = width
         self.total_rows = total_rows
@@ -48,7 +48,7 @@ class Spot:
 
     #obstacle
     def is_barrier(self):
-        return self.color == BLACK
+        return self.color == WHITE
 
     def is_start(self):
         return self.color == ORANGE
@@ -57,7 +57,7 @@ class Spot:
         return self.color == PURPLE
 
     def reset(self):
-        self.color = WHITE
+        self.color = BLACK
 
     def make_start(self):
         self.color = ORANGE
@@ -69,7 +69,7 @@ class Spot:
         self.color = GREEN
 
     def make_barrier(self):
-        self.color = BLACK
+        self.color = WHITE
 
     def make_end(self):
         self.color = TURQUOISE
@@ -81,7 +81,21 @@ class Spot:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        #check up down left right 
+        #if not barrier then add
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): #down
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): #up
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): #right
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): #left
+            self.neighbors.append(grid[self.row][self.col - 1])
+
 
     #lt = less than
     def __lt__(self, other):
@@ -92,6 +106,65 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return  abs(x1 - x2) + abs(y1 - y2)
+
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+    
+
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {} #keep track of path
+    g_score = {spot: float("inf") for row in grid for spot in row} 
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row} 
+    f_score[start] = h(start.get_pos(), end.get_pos()) #set to heuristic
+
+    open_set_hash = {start} #check if anything is in priorityqueue
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+
+        #consider all neighbors
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
+
+
+
 
 #hold all spots in grid
 def make_grid(rows, width):
@@ -113,7 +186,7 @@ def draw_grid(win, rows, width):
             pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
 
 def draw(win, grid, rows, width):
-    win.fill(WHITE)
+    win.fill(BLACK)
 
     for row in grid:
         for spot in row:
@@ -134,7 +207,7 @@ def get_clicked_pos(pos, rows, width):
 
 #main loop
 def main(win, width):
-    ROWS = 50 # 50 x 50 grid can be changed
+    ROWS = 25 # 50 x 50 grid can be changed
     grid = make_grid(ROWS, width)
 
     start = None
@@ -155,10 +228,9 @@ def main(win, width):
             # 0 == left mouse button, 1 == middle  2 == right mouse 
             if pygame.mouse.get_pressed()[0]: #LEFT
                 pos = pygame.mouse.get_pos()
-                #print('Mouse positon: ', pos)
                 row, col = get_clicked_pos(pos, ROWS, width)
-                #print('Row, col: ', row, col)
                 spot = grid[row][col]
+                # do not allow spot to overide start/end
                 if not start and spot != end:
                     start = spot
                     start.make_start()
@@ -171,7 +243,30 @@ def main(win, width):
                     spot.make_barrier()
 
             elif pygame.mouse.get_pressed()[2]: #RIGHT
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                elif spot == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+                    
+                    #after updating neighbors call algorithm
+                    #passing a function equal to a function call
+                    #Lambda is an anonoymous function
+                    algorithm(lambda : draw(win, grid, ROWS, width), grid, start, end)
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
 
     pygame.quit()
 
